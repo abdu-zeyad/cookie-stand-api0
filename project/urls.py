53eb828fde6f15ce6171666be93bf5927b439abd
Abdelmajed-zeyad <abd.d@live.com>
2021-09-08 19:54:44 +0300

    change url

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import kill
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
@@ -32,3 +33,6 @@ urlpatterns = [
         name="token_refresh",
     ),
 ]
+
+
+
